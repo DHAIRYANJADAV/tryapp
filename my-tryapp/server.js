3cfe28bd016c47b315cf700d6416cd7448ea0561
DHAIRYANJADAV <dhairyanjadav21@gnu.ac.in>
2025-01-31 11:09:53 +0530

    Update project files

--- my-tryapp/server.js
+++ my-tryapp/server.js
@@ -2,11 +2,16 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+// Serve static files from 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Route for the homepage
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'egsite.html'));
 });
 
+// Use 0.0.0.0 to listen on all interfaces
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on http://0.0.0.0:${PORT}`);
+});
